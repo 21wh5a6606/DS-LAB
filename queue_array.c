3947ce56e4dfc4e5f652751efca724f52949743d
21wh5a6606 <93035370+21wh5a6606@users.noreply.github.com>
2021-12-17 16:13:42 +0530

    Add files via upload

---
+++ queue_array.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 5
+int front = -1,rear = -1,a[size];
+void enqueue(int ele)
+{
+a[++rear] = ele;
+if(front == -1)
+    front = 0;
+}
+int dequeue()
+{
+     if(front == rear)
+    	 return a[front];
+     	 front = rear = -1;
+     else
+    	 return a[front];
+         front ++;
+}
+void display()
+{
+    for(int i = front;i <= rear; i++)
+    printf("%d\n",a[i]);
+}
+int main()
+{
+    int ch,element;
+    while(1)
+    {
+        printf("1-enqueue\n 2-dequeue\n 3-display\n 4-exit\n");
+        scanf("%d",&ch);
+    switch(ch)
+    {
+    case 1:if(rear == size-1)
+               printf("queue overflow");
+           else
+                printf("enter element to be inserted");
+                scanf("%d",&element);
+                enqueue(element);
+                break;
+                break;
+    case 2:if(front = -1)
+		 printf("queue underflow");
+            else
+                printf("deleted element is %d",dequeue());
+                break;
+    case 3:if(front == -1)
+                printf("queue empty");
+           else
+                display();
+                break;
+    case 4:exit(0);
+}
+}
+}
+
+
