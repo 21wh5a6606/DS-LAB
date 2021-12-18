3ec188c6db9339ce39bba920aa71152499b27a5e
21wh5a6606 <93035370+21wh5a6606@users.noreply.github.com>
2021-12-18 23:52:41 +0530

    Add files via upload

---
+++ queue_linkedlist.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h> 
+struct node
+{
+	int data;
+	struct node*link;
+};
+struct node *front = NULL,*rear = NULL,*cur;
+void enqueue()
+{
+	cur = (struct node*)malloc(sizeof(struct node));
+		printf("enter current data");
+		scanf("%d",&cur -> data);
+		cur -> link = NULL;
+	if(front == NULL)
+		front = rear = cur;
+	else
+		rear -> link = cur;
+		rear = cur;
+}
+void dequeue()
+{
+	if(front == NULL)
+		printf("queue empty");
+	else if(front == rear)
+	{
+		printf("deleted element is %d",front -> data);
+		front = rear = NULL;
+	}
+	else
+		printf("deleted element is %d",front -> data);
+		front = front-> link;
+}
+void display()
+{
+	if(front == NULL)
+		printf("queue is empty");
+	else
+		cur = front;
+		while(cur != NULL)
+		{
+			printf("%d",cur -> data);
+			cur = cur -> link;
+		}
+}
+int main()
+{
+	int ch,element;
+	while(1)
+	{
+		printf("1-enqueue\n 2-dequeue\n 3-display\n 4-exit\n");
+		printf("enter your choice");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:enqueue();
+				   break;
+			case 2:dequeue();
+				   break;
+			case 3:display();
+				   break;
+			case 4:exit(0);
+		}
+	}
+}
